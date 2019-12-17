f1a7dde62ed81b3a9b97557b8ce32cb93a411939
Justin White <kyjus25@gmail.com>
2019-12-17 14:25:28 -0600

    Initial commit

---
+++ docker-harpoon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Remove Containers
+if [ "$(docker ps -a -q)" != "" ]; then
+	docker stop $(docker ps -a -q)
+	docker rm $(docker ps -a -q)
+fi
+
+#Remove Images
+if [ "$(docker image list -a -q)" != "" ]; then
+	docker image stop $(docker image list -a -q) --force
+	docker image rm $(docker image list -a -q) --force
+fi
+
+#Remove Volumes
+if [ "$(docker volume list -q)" != "" ]; then
+	docker volume stop $(docker volume list -q) --force
+	docker volume rm $(docker volume list -q) --force
+fi
